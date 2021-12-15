06a613554a9c376937a7c1e3468b71ae8088b65a
hrd9 <hrd9@njit.edu>
2021-12-15 10:57:49 -0500

    Project 3 : Integration of flask, tables and Project 1 & 2

--- app/controllers/calculator_controller.py
+++ app/controllers/calculator_controller.py
@@ -1,7 +1,7 @@
 from app.controllers.controller import ControllerBase
 from projectcalculator.maincalculator import Calculator
 from flask import render_template, request, flash, redirect, url_for
-
+import pandas as pd
 
 class CalculatorController(ControllerBase):
     @staticmethod
@@ -18,6 +18,8 @@ class CalculatorController(ControllerBase):
             flash('The value of field 1 needs to be inserted')
         elif request.form['value2'] == '':
             flash('The value of field 2 needs to be inserted')
+        elif request.form['value2'] == '0' and operation == 'division' :
+            flash('Division by Zero Exception occurred !!')
         else:
             flash('Operation performed successfully !!!!')
 
@@ -26,7 +28,15 @@ class CalculatorController(ControllerBase):
             # this will call the correct operation
             getattr(Calculator, operation)(my_tuple)
             result = str(Calculator.get_last_value())
-            return render_template('result.html', value1=value1, value2=value2, operation=operation, result=result)
+
+            #CSV Write logic
+            calc_obj = {'Value1': [value1], 'Value2': [value2], 'Operation': [operation], 'Result': [result]}
+            df = pd.Dataframe(calc_obj, columns=['Value1', 'Value2', 'Operation', 'Result'])
+            df.to_csv('projectoutput.csv', mode='a', index=False, header=False, sep=',')
+
+            hist = pd.read_csv('projectoutput.csv', skiprows=1).values.tolist()
+
+            return render_template('result.html', value1=value1, value2=value2, operation=operation, result=result, hist=hist)
         return render_template('basicform.html')
 
     @staticmethod
